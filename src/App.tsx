7fcb25bd41a10716c3fb773dc69d885e4d62aa5a
DM-MRTNVCH <dmvshn@gmail.com>
2020-12-14 22:43:57 +0300

    new components: AccordionTitle, AccordionBody AppTitle - props.title, Rating - props.value

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from './components/Accordion';
+import {Rating} from './components/Rating/Rating';
 
 
 function App() {
-    console.log('Appf rendering')
+    console.log('App rendering')
     return (
         <div className="App">
-            <AppTitle/>
-            <Rating/>
+            <PageTitle title={'This is APP component'}/>
+            Article 1
+            <Rating value={3}/>
             <Accordion/>
-            <Rating/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
-    console.log('AppTitle rendering')
-    return <div>This is APP component</div>
+function PageTitle(props: any) {
+    console.log('PageTitle rendering')
+    return <h1>{props.title}</h1>
 }
 
-
-function Rating() {
-    console.log('Rating rendering')
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    )
-}
-
-
-function Star() {
-    console.log('Star rendering')
-    return <div>star</div>
-}
-
-
-function Accordion() {
-    console.log('Accordion rendering')
-    return (
-        <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    )
-}
 
 export default App;
