99b19215e6f67a016de44cd7cffe71f3efdffcee
DM-MRTNVCH <dmvshn@gmail.com>
2020-12-18 20:42:41 +0300

    useState - OnOff.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import React from 'react';
 import './App.css';
 import Accordion from './components/Accordion';
 import {Rating} from './components/Rating/Rating';
+import OnOff from './components/OnOff/OnOff';
 
 
 function App() {
     console.log('App rendering')
     return (
         <div className="App">
-            <PageTitle title={'This is APP component'}/>
-            Article 1
-            <Rating value={5}/>
-            <Accordion titleValue={'Меню'} collapsed={true}/>
-            <Accordion titleValue={'раз'} collapsed={false}/>
-            Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            {/*<PageTitle title={'This is APP component'}/>*/}
+            {/*Article 1*/}
+            {/*<Rating value={5}/>*/}
+            {/*<Accordion titleValue={'Меню'} collapsed={true}/>*/}
+            {/*<Accordion titleValue={'раз'} collapsed={false}/>*/}
+            {/*Article 2*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+
+            <OnOff/>
         </div>
     );
 }
---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+
+type OnOffType = {
+    // on: boolean
+}
+
+
+function OnOff (props: OnOffType) {
+
+    let [on, setOn] = useState(false);
+
+    const onStyle = {
+        height: "20px",
+        width: "40px",
+        border: "1px solid black",
+        display: "inline-block",
+        backgroundColor: on ? "green" : "white"
+    }
+    const offStyle = {
+        height: "20px",
+        width: "40px",
+        border: "1px solid black",
+        display: "inline-block",
+        margin: "2px",
+        backgroundColor: on ? "white" : "red"
+    }
+    const indicatorStyle = {
+        height: "12px",
+        width: "12px",
+        borderRadius: "8px",
+        border: "1px solid black",
+        display: "inline-block",
+        backgroundColor: on ? "green" : "red"
+    }
+
+    return <div>
+        <div style={onStyle} onClick={() => {setOn(true)}}>On</div>
+        <div style={offStyle} onClick={() => {setOn(false)}}>Off</div>
+        <div style={indicatorStyle}></div>
+    </div>
+}
+
+export default OnOff;
