e256aff120c07ee3e86701940c339cd025f1f011
DM-MRTNVCH <dmvshn@gmail.com>
2021-01-17 23:35:33 +0300

    useReducer

---
+++ src/components/UncontrolledAccordion/Reducer.test.tsx
@@ -0,0 +1,35 @@
+import {StateType, reducer} from './Reducer'
+
+test('reducer should change value to oposite one', () => {
+
+    const state: StateType = {
+        collapsed: false
+    }
+
+    const newState = reducer(state, {type: TOGGLE_COLLAPSED})
+
+    expect(newState.collapsed).toBe(true)
+})
+
+test('collapsed should be true', () => {
+
+    const state: StateType = {
+        collapsed: false
+    }
+
+    const newState = reducer(state, {type: TOGGLE_COLLAPSED})
+
+    expect(newState.collapsed).toBe(false)
+})
+
+test('reducer should show error', () => {
+
+    const state: StateType = {
+        collapsed: true
+    }
+
+    expect(() => {
+        reducer(state, {type: 'FAKE'})
+    }).toThrowError()
+})
+
---
+++ src/components/UncontrolledAccordion/Reducer.tsx
@@ -0,0 +1,22 @@
+export type ActionType = {
+    type: string
+}
+
+export const TOGGLE_CONSTANT = 'TOGGLE_COLLAPSED'
+
+export type StateType = {
+    collapsed: boolean
+}
+
+export const reducer = (state: StateType, action: ActionType): StateType => {
+    switch (action.type) {
+        case TOGGLE_CONSTANT:
+            return {
+                ...state,
+                collapsed: !state.collapsed
+            }
+        default:
+            throw new Error('Bac action type')
+    }
+    return state
+}
