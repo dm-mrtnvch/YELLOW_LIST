ffc01b26379836f745673e91e923f5d6055edff3
DM-MRTNVCH <dmvshn@gmail.com>
2021-05-07 19:18:02 +0300

    useEffect, setTimeout, setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
 export default {
-    title: 'UseEffect demo'
+    title: 'UseEffectDemo'
 }
 
 
@@ -9,31 +9,50 @@ export const SimpleExample = () => {
 
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
-    console.log('simple example')
+    console.log('set timeout example')
+
+    // useEffect(() => {
+    //     console.log('simple useEffect')
+    //     document.title = counter.toString()
+    // })
+    //
+    // useEffect(() => {
+    //     console.log('useEffect render every time')
+    //     document.title = counter.toString()
+    // })
+    //
+    // useEffect(() => {
+    //     console.log('useEffect render first time')
+    //     document.title = counter.toString()
+    // }, [])
+    //
+    // useEffect(() => {
+    //     console.log('useEffect render every counter changes')
+    //     document.title = counter.toString()
+    // }, [counter])
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log('use effect with counter')
+    //         document.title = counter.toString()
+    //     }, 1000)
+    // }, [counter])
 
     useEffect(() => {
-        console.log('simple useEffect')
-        document.title = counter.toString()
-    })
+        setInterval(() => {
+            console.log('tick: ' + counter)
+            setCounter(state => state + 1)
+        }, 1000)
+    },[])
 
-    useEffect(() => {
-        console.log('useEffect render every time')
-        document.title = counter.toString()
-    })
 
-    useEffect(() => {
-        console.log('useEffect render first time')
-        document.title = counter.toString()
-    }, [])
 
-    useEffect(() => {
-        console.log('useEffect render every counter changes')
-        document.title = counter.toString()
-    }, [counter])
+
 
     return <>
         hello, {counter} {fake}
-        <button onClick={() => setFake(fake + 1)}>+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter +</button>
+        <button onClick={() => setFake(fake + 1)}>fake +</button>
     </>
 
 }
