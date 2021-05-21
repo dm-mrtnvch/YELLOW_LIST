7006496d51a5cb16f39fb83cc16b99a5c878a2bc
DM-MRTNVCH <dmvshn@gmail.com>
2021-05-21 11:01:15 +0300

    useEffect, cleanup

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -5,42 +5,73 @@ export default {
 }
 
 
+export const ResetEffectExample = () => {
+
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+    console.log('component rendered')
+
+
+
+    useEffect(() => {
+        console.log('effect ocured: ' + counter)
+        return () => {
+            console.log('reset effect: ' + counter)
+        }
+    },[counter])
+
+
+    const increase = () => setCounter(counter + 1)
+
+
+    return <>
+        Hello, counter: {counter} <button onClick={increase}>++++</button>
+    </>
+
+}
+
+
+
+export const KeysTrackerExample = () => {
+
+    const [text, setText] = useState('')
+
+    console.log('component rendered with' + text)
+
+
+
+    useEffect(() => {
+
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+
+        window.addEventListener('keypress', handler)
+
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    },[text])
+
+    return <>
+        Typed text: {text}
+    </>
+
+}
+
+
+
 export const SimpleExample = () => {
 
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
     console.log('set timeout example')
 
-    // useEffect(() => {
-    //     console.log('simple useEffect')
-    //     document.title = counter.toString()
-    // })
-    //
-    // useEffect(() => {
-    //     console.log('useEffect render every time')
-    //     document.title = counter.toString()
-    // })
-    //
-    // useEffect(() => {
-    //     console.log('useEffect render first time')
-    //     document.title = counter.toString()
-    // }, [])
-    //
-    // useEffect(() => {
-    //     console.log('useEffect render every counter changes')
-    //     document.title = counter.toString()
-    // }, [counter])
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         console.log('use effect with counter')
-    //         document.title = counter.toString()
-    //     }, 1000)
-    // }, [counter])
 
     useEffect(() => {
         setInterval(() => {
-            console.log('tick: ' + counter)
+            // console.log('tick: ' + counter)
             setCounter(state => state + 1)
         }, 1000)
     },[])
@@ -56,3 +87,94 @@ export const SimpleExample = () => {
     </>
 
 }
+
+
+
+
+export const SetTimeoutExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('component rendered with' + text)
+
+
+
+    useEffect(() => {
+
+        const timeoutID = setTimeout(() => {
+            console.log('TIMEOUT EXPIRED')
+            setText(' 3 seconds passed')
+        }, 3000)
+
+        return () => {
+            clearTimeout(timeoutID)
+        }
+    },[text])
+
+    return <>
+        Typed text: {text}
+    </>
+
+}
+
+
+
+
+export const SetIntervalExample = () => {
+    const [text, setText] = useState('')
+
+    console.log('component rendered with' + text)
+
+
+
+    useEffect(() => {
+
+        const intervalID = setInterval(() => {
+            setText(' 3 seconds passed')
+        }, 3000)
+
+        return () => {
+            clearInterval(intervalID)
+        }
+    },[text])
+
+    return <>
+        Typed text: {text}
+    </>
+
+}
+
+
+
+
+
+
+
+
+
+
+// useEffect(() => {
+//     console.log('simple useEffect')
+//     document.title = counter.toString()
+// })
+//
+// useEffect(() => {
+//     console.log('useEffect render every time')
+//     document.title = counter.toString()
+// })
+//
+// useEffect(() => {
+//     console.log('useEffect render first time')
+//     document.title = counter.toString()
+// }, [])
+//
+// useEffect(() => {
+//     console.log('useEffect render every counter changes')
+//     document.title = counter.toString()
+// }, [counter])
+
+// useEffect(() => {
+//     setTimeout(() => {
+//         console.log('use effect with counter')
+//         document.title = counter.toString()
+//     }, 1000)
+// }, [counter])
